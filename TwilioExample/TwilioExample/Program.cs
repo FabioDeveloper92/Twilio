1cc87141394bff1e707ff86dfb4362c62693ce88
FabioR <faitel92@hotmail.it>
2023-01-28 13:43:42 +0100

    add .net project
    It's a simple example

---
+++ TwilioExample/TwilioExample/Program.cs
@@ -0,0 +1,15 @@
+﻿using TwilioExample.Service;
+
+var twilioAccountSid = "";
+var twilioAuthToken = "";
+var fromPhone = "";
+var serviceId = "";
+var toPhoneNumber = "";
+
+var twilioService = new TwilioService(twilioAccountSid, twilioAuthToken, fromPhone, serviceId);
+twilioService.SendSimpleMessage(toPhoneNumber, "Test with service");
+
+twilioService.SendVerificationMessage(toPhoneNumber);
+
+var myCode = "123456";
+twilioService.CheckVerificationMessage(toPhoneNumber, myCode);
---
+++ TwilioExample/TwilioExample/Service/TwilioService.cs
@@ -0,0 +1,56 @@
+﻿using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Rest.Verify.V2.Service;
+
+namespace TwilioExample.Service
+{
+    public class TwilioService
+    {
+        private readonly string _accountSid;
+        private readonly string _authToken;
+        private readonly string _fromPhoneNumber;
+        private readonly string _serviceId;
+
+        public TwilioService(string accountSid, string authToken, string fromPhoneNumber, string serviceId)
+        {
+            _accountSid = accountSid;
+            _authToken = authToken;
+            _fromPhoneNumber = fromPhoneNumber;
+            _serviceId = serviceId;
+            TwilioClient.Init(_accountSid, _authToken);
+        }
+
+        public MessageResource SendSimpleMessage(string toPhoneNumber, string body)
+        {
+            var message = MessageResource.Create(
+                body: body,
+                from: new Twilio.Types.PhoneNumber(_fromPhoneNumber),
+                to: new Twilio.Types.PhoneNumber(toPhoneNumber)
+            );
+
+            return message;
+        }
+
+        public string SendVerificationMessage(string toPhoneNumber)
+        {
+            var verification = VerificationResource.Create(
+                to: toPhoneNumber,
+                channel: "sms",
+                pathServiceSid: _serviceId
+            );
+
+            return verification.Status;
+        }
+
+        public string CheckVerificationMessage(string toPhoneNumber, string code)
+        {
+            var verificationCheck = VerificationCheckResource.Create(
+                to: toPhoneNumber,
+                code: code,
+                pathServiceSid: _serviceId
+            );
+
+            return verificationCheck.Status;
+        }
+    }
+}
